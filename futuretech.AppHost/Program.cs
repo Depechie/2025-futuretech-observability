dc22dadd09a5713d677c7cfd6630e5d7748e387f
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2025-05-15 10:07:59 +0200

    Add redis cache

--- futuretech.AppHost/Program.cs
+++ futuretech.AppHost/Program.cs
@@ -1,9 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.futuretech_ApiService>("apiservice");
+var cache = builder.AddRedis("cache")
+    .WithRedisInsight();
+
+var apiService = builder.AddProject<Projects.futuretech_ApiService>("apiservice")
+    .WithReference(cache)
+    .WaitFor(cache);
 
 builder.AddProject<Projects.futuretech_Web>("webfrontend")
     .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService);
 
